2bd9608bf80d451e7ab39cdaae505fe857eefdd7
logesh8220 <logeshthirumurugan@gmail.com>
2023-01-29 13:28:40 +0530

    added projects

--- src/About.js
+++ src/About.js
@@ -7,7 +7,7 @@ function About() {
                 <div className="about-div-2" >
                     <div className="container text-light py-5 col-lg-5 col-md-8 col-sm-10 " >
                         <h2 className="my-5" style={{fontFamily: "Cairo, sans-serif "}}>Hi,I'M logesh.Nice to meet you.</h2>
-                        <h5 className="about-text fw-bolder"> I Change my Carrier Path in BBA to Web Developer.
+                        <h5 className="about-text fw-bolder">
                             I am a self-starter with strong interpersonal skills. I completed Full-Stack[MERN] Developer Bootcamp @ Guvi,Chennai.
                             I have hands-on experience in developing Full-Stack Web Application wtih MERN stack.I am passionate about building web Applications and eager to learn new things around software Development.
                         </h5>
--- src/Project.js
+++ src/Project.js
@@ -16,42 +16,39 @@ function Project() {
             <div className="container project-container text-light text-center"  >
                 <div className="row justify-content-around my-5">
                     <div className=" project1  col-lg-4">
-                        <img src="/img/urlshortner.png" className="card-img-top project-img" alt="urlshortner"/>
-                            <div >
-                                <p style={{ fontFamily: "Cairo, sans-serif" }}>This tool allows you to seamlessly track your audience with simple and easy-to-remember yet powerful links and provide your customers a unique tailored experience</p>
-                                <div className='text-muted'>
-                                <h6>Email:loganguvitest@gmail.com </h6>
-                                <h6>pass:Dfsdfgsdfs34s</h6>
-                                </div>
-                                <a className='p-icon' href='https://633d244cce5a75000919bc88--imaginative-lokum-bba323.netlify.app/' rel="noreferrer" target="_blank"><i className="bi bi-box-arrow-up-right me-4 p-4 project-icon"></i></a>
-                                <a className='p-icon' href='https://github.com/logesh8220/url_shortner_react' rel="noreferrer" target="_blank"><i className="bi bi-github ms-4 p-4 project-icon"></i></a>
-                            </div>
+                        <img src="/img/zen.png" className="card-img-top project-img" alt="urlshortner" />
+                        <div >
+                            <p style={{ fontFamily: "Cairo, sans-serif" }}>Built with Bootstrap v5. Implemented Role Based Access Rights eg – (Mentor, Student) each user have their own features and limited access. Can.
+                                App also includes Product CRUD .</p>
+                            <a className='p-icon' href='https://zenclass-dashboard-clone-batch37.netlify.app' rel="noreferrer" target="_blank"><i className="bi bi-box-arrow-up-right me-4 p-4 project-icon"></i></a>
+                            <a className='p-icon' href='https://github.com/logesh8220/zen-class-dashbord' rel="noreferrer" target="_blank"><i className="bi bi-github ms-4 p-4 project-icon"></i></a>
+                        </div>
                     </div>
                     <div className=" project1  col-lg-4">
-                        <img src="/img/youtubedownloder.png" className="card-img-top" alt="urlshortner"/>
-                            <div >
-                                <p style={{ fontFamily: "Cairo, sans-serif" }}>With My YouTube video downloader, you can select videos and download them free of charge! You can get high-quality videos in MP4, MP3, SQ, HD, Full HD quality, and other formats. Our software works both on PC and mobile, allowing you to use it anywhere you are!</p>
-                                <a className='p-icon' href='hhttps://master--cozy-twilight-74a3ef.netlify.app/' rel="noreferrer" target="_blank"><i className="bi bi-box-arrow-up-right me-4 p-4 project-icon"></i></a>
-                                <a className='p-icon' href='https://github.com/logesh8220/youtube-video-downloader-react'><i className="bi bi-github ms-4 p-4 project-icon"></i></a>
-                            </div>
+                        <img src="/img/youtubedownloder.png" className="card-img-top project-img" alt="youtubedownloader" />
+                        <div >
+                            <p style={{ fontFamily: "Cairo, sans-serif" }}>With My YouTube video downloader, you can select videos and download them free of charge! You can get high-quality videos in MP4, MP3, SQ, HD, Full HD quality, and other formats. Our software works both on PC and mobile, allowing you to use it anywhere you are!</p>
+                            <a className='p-icon' href='https://master--cozy-twilight-74a3ef.netlify.app/' rel="noreferrer" target="_blank"><i className="bi bi-box-arrow-up-right me-4 p-4 project-icon"></i></a>
+                            <a className='p-icon' href='https://github.com/logesh8220/youtube-video-downloader-react' rel="noreferrer" target="_blank"><i className="bi bi-github ms-4 p-4 project-icon"></i></a>
+                        </div>
                     </div>
                 </div>
                 <div className="row justify-content-around my-5">
                     <div className=" project1  col-lg-4">
-                        <img src="/img/imdbclone.png" className="card-img-top project-img" alt="urlshortner"/>
-                            <div >
-                                <p style={{ fontFamily: "Cairo, sans-serif" }}>This is IMDB clone its show the online movies and TV shows ratings and other details with in short span of time with limited contents</p>
-                                <a className='p-icon' href='https://heroic-kulfi-6e4aa1.netlify.app/' rel="noreferrer" target="_blank"><i className="bi bi-box-arrow-up-right me-4 p-4 project-icon"></i></a>
-                                <a className='p-icon' href='https://github.com/logesh8220/Movie-details-IMDb'><i className="bi bi-github ms-4 p-4 project-icon"></i></a>
-                            </div>
+                        <img src="/img/realmestore.png" className="card-img-top project-img" alt="urlshortner" />
+                        <div >
+                            <p style={{ fontFamily: "Cairo, sans-serif" }}>Built with Bootstrap v5. Implemented ADMIN portal and integrated RAZORPAY payment Integration to make order from cart. All Products are created in Admin Portal of App </p>
+                            <a className='p-icon' href='https://neon-otter-16f6a4.netlify.app/' rel="noreferrer" target="_blank"><i className="bi bi-box-arrow-up-right me-4 p-4 project-icon"></i></a>
+                            <a className='p-icon' href='https://github.com/logesh8220/Realme-store' rel="noreferrer" target="_blank"><i className="bi bi-github ms-4 p-4 project-icon"></i></a>
+                        </div>
                     </div>
                     <div className=" project1  col-lg-4">
-                        <img src="/img/shortener1.png" className="card-img-top" alt="urlshortner"/>
-                            <div >
-                                <p style={{ fontFamily: "Cairo, sans-serif" }}>Comming Soon...</p>
-                                <a className='p-icon' href='https://633d244cce5a75000919bc88--imaginative-lokum-bba323.netlify.app/' rel="noreferrer" target="_blank"><i className="bi bi-box-arrow-up-right me-4 p-4 project-icon"></i></a>
-                                <a className='p-icon' href='https://github.com/logesh8220/Movie-details-IMDb'><i className="bi bi-github ms-4 p-4 project-icon"></i></a>
-                            </div>
+                        <img src="/img/crm.png" className="card-img-top project-img" alt="urlshortner" />
+                        <div >
+                            <p style={{ fontFamily: "Cairo, sans-serif" }}> Built with Bootstrap v5. Implemented Role Based Access (ADMIN, Employee and Customers). each user have their own features and limited access. Can. App also includes Product CRUD </p>
+                            <a className='p-icon' href='https://funny-sprite-477a4b.netlify.app/' rel="noreferrer" target="_blank"><i className="bi bi-box-arrow-up-right me-4 p-4 project-icon"></i></a>
+                            <a className='p-icon' href='https://github.com/logesh8220/WebCode-2-CRM' rel="noreferrer" target="_blank"><i className="bi bi-github ms-4 p-4 project-icon"></i></a>
+                        </div>
                     </div>
                 </div>
             </div>
